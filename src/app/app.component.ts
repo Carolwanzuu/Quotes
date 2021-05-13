c8ef5113ef9930b5f581d70fcb50b9200ac6c991
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-13 15:31:13 +0300

    edited app template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
+  quotes:Quote[]=[
+    {id:1, name:'Anais Nin', description:'We write to taste life twice, in the moment and in retrospect'},
+    {id:2, name:'Mark Twain', description:'One day I will find the right words, and they will be simple.'},
+    {id:3, name:'Robert Frost', description:'No tears in the writer, no tears in the reader.'},
+    {id:4, name:'Henry David', description:'How vain it is to sit down to write when you have not stood up to live.'},
+  ];
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    id:number;
+    name:string;
+    description:string;
+    // constructor(public id:number, public name:string, public description:string)
 }
