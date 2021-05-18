0074af2b30203c860992aa8e6191f538a7475544
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-18 10:45:17 +0300

    added a background image to style.css

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -37,16 +37,16 @@ export class QuotesComponent implements OnInit {
       this.quotes.push(quote)
     }
   
-    upVotes:number=0;
-    upVote(){
-      this.upVotes=this.upVotes+1;
+    // upVotes:number=0;
+    // upVote(){
+    //   this.upVotes=this.upVotes+1;
 
-    }
+    // }
 
-    downVotes:number=0;
-    downVote(){
-      this.downVotes=this.downVotes-1;
-    }
+    // downVotes:number=0;
+    // downVote(){
+    //   this.downVotes=this.downVotes-1;
+    // }
   constructor() { }
 
   ngOnInit(): void {
